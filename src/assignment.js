e98dc52d5d29021dbd62e6f79ea2d16d1921ee9e
Moses Adebayo <94234833+muzardemoses@users.noreply.github.com>
2022-07-11 10:26:45 +0000

    implemented even numbers count and return sum of numbers in an array

--- src/assignment.js
+++ src/assignment.js
@@ -14,9 +14,10 @@ const assignment = {};
  * @returns number the sum of the numbers
  */
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+   const result = arrayOfNumbers.reduce((acc, curr) => acc + curr, 0);
+    return result
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -28,9 +29,10 @@ function sumOfNumbers(arrayOfNumbers) {
  * @returns number the count of even numbers
  */
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+  const filteredNumbers = arrayOfNumbers.filter(number => number % 2 === 0);
+    return filteredNumbers.length;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
